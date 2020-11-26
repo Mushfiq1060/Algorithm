8532f5c45d85a1f8c9ba1f4c14ed40119ff3ff8f
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2020-11-26 10:59:06 +0600

    Update Sparse Table.cpp

--- Sparse Table.cpp
+++ Sparse Table.cpp
@@ -28,6 +28,7 @@ int main()
     **/
    
    /** Print Sparse Table**/
+    cout<<"Sparse Table : "<<endl;
     for(int i=0;i<n;i++,cout<<endl)
         for(int j=0;j<=k;j++)   
             cout<<st[i][j]<<" ";
