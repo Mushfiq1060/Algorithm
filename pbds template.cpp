99eb753589da4cd62c67d113beda11bb147db258
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2021-05-29 20:01:17 +0600

    Add files via upload

---
+++ pbds template.cpp
@@ -0,0 +1,5 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds ;
+template <typename PB>
+using ordered_set = tree<PB,null_type,less<PB>,rb_tree_tag,tree_order_statistics_node_update> ;
