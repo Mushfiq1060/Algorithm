94f0ebb7a35288e03a7c0458a3a7424b85aa5ebc
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2021-06-20 00:31:28 +0600

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,18 @@
+/// Written function inside the main function
+
+/*
+
+function(return type(data types of function parameters))function name = [&](function parameter)
+{
+
+};
+
+*/
+
+function(int(int,int))get_sum=[&](int a,int b)
+{
+    return a+b;
+};
+
+
+
