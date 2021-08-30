ed2efaf5ae64008f853a331907a3940c3733a019
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2021-08-30 15:08:53 +0600

    Update euler phi.cpp

--- euler phi.cpp
+++ euler phi.cpp
@@ -4,15 +4,14 @@ const int mxn=1e5+10;
 int phi[mxn];
 void phi_sieve()
 {
-    for(int i=1;i<mxn;i++)
-        phi[i]=i;
-    for(int i=2;i<mxn;i++)
-        if(phi[i]==i)
-        {
-            phi[i]=i-1;
-            for(int j=i+i;j<mxn;j+=i)
-                phi[j]=(phi[j]*(i-1))/i;
-        }
+  for(int i=1;i<mxn;i++) 
+    phi[i]=i;
+  for(int i=2;i<mxn;i++)
+    if(phi[i]==i)
+    {
+      for(int j=i;j<mxn;j+=i)
+        phi[j]-=phi[j]/i;
+    }
 }
 int main()
 {
