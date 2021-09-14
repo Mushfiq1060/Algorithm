bbfa4748e1a747fde5c25308f5accb529aa04f54
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2021-09-14 13:17:14 +0600

    Update pbds template.cpp

--- pbds template.cpp
+++ pbds template.cpp
@@ -1,6 +1,8 @@
 /** it does not cantains any duplicate value ***/
+#include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
 using namespace __gnu_pbds ;
 template <typename PB>
 using ordered_set = tree<PB,null_type,less<PB>,rb_tree_tag,tree_order_statistics_node_update> ;
@@ -11,8 +13,10 @@ using ordered_set = tree<PB,null_type,less<PB>,rb_tree_tag,tree_order_statistics
 
 /** if we use less_equal then ordered set contains duplicate value and upperbound act as
     lower bound and lower bound act as upperbound ***/
+#include<bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
 using namespace __gnu_pbds ;
 template <typename PB>
 using ordered_set = tree<PB,null_type,less_equal<PB>,rb_tree_tag,tree_order_statistics_node_update> ;
