1dba2d391ddaed69b1c73363a76e31a497008b6c
Mushfiq1060 <60137904+Mushfiq1060@users.noreply.github.com>
2022-04-25 13:31:12 +0600

    Update function inside the main function.cpp

--- function inside the main function.cpp
+++ function inside the main function.cpp
@@ -9,10 +9,16 @@ function(return type(data types of function parameters))function name = [&](func
 
 */
 
-function(int(int,int))get_sum=[&](int a,int b)
-{
-    return a+b;
+function<int(int,int)>get_sum=[&](int a,int b){
+  return a+b;
 };
 
-
+function<void(int,int,int)>dfs=[&](int src,int par,int lev){
+  res=max(res,lev);
+  for(int i=0;i<g[src].size();i++){
+    if(g[src][i]!=par){
+      dfs(g[src][i],src,lev+1);
+    }
+  }
+};
 
